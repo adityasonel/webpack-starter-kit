c80a9688d25774b3a74b14a81218263114b9d4c5
adityasonel <aditya.sonel@gmail.com>
2020-10-12 22:53:12 +0530

    g-tag added

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const buildPath = path.resolve(__dirname, "dist");
+const buildPath = path.resolve(__dirname, "docs");
 
 module.exports = {
 	devtool: "source-map",
